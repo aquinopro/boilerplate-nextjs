28c4edb35c42bf4616660f8a80ac46b93ce33fa6
Guilherme <guI@aquino.ind.br>
2020-12-13 17:43:15 -0300

    teste

---
+++ src/components/Main/index.tsx
@@ -0,0 +1,5 @@
+export const Main = () => (
+    <main>
+        <h2>Meu texto top</h2>
+    </main>
+);
---
+++ src/components/Main/test.tsx
@@ -0,0 +1,10 @@
+import { render, fireEvent, screen } from '@testing-library/react';
+import { Main } from '.';
+
+describe('<Main />', () => {
+    it('shoud render the heading', () => {
+        const { container } = render(<Main />);
+        expect(screen.getByRole('heading', { name: /meu texto top/i })).toBeInTheDocument();
+        // expect(container.firstChild).toMatchSnapshot();
+    });
+});
